5d517f824c9abe4dd88365519cac6d2ca42bc0ff
pavel <pavkascool@gmail.com>
2020-03-23 06:57:08 +0300

    complete a few more tasks

--- src/com/company/Task1_OneDimensionalArray.java
+++ src/com/company/Task1_OneDimensionalArray.java
@@ -24,6 +24,44 @@ public class Task1_OneDimensionalArray {
         return count;
     }
 
+    //This method calculates number of negative, neutral and positive elements in an array of doubles
+    public static int[] calculateMembers(double[] array) {
+
+        int negative = 0;
+        int neutral = 0;
+        int positive = 0;
+        for(double element: array) {
+            if(element < 0) negative++;
+            else if(element == 0) neutral++;
+            else positive++;
+        }
+
+        return new int[]{negative, neutral, positive};
+    }
+
+    //This method exchanges min and max values in the array
+    public static void exchangeMinMax(double[] array) {
+        System.out.println(Arrays.toString(array));
+        double min = array[0];
+        int minIndex = 0;
+        double max = array[0];
+        int maxIndex = 0;
+        for(int i = 1; i < array.length; i++) {
+            if(array[i] < min) {
+                minIndex = i;
+                min = array[i];
+            }
+            if(array[i] > max) {
+                maxIndex = i;
+                max = array[i];
+            }
+        }
+        array[minIndex] = max;
+        array[maxIndex] = min;
+
+        System.out.println(Arrays.toString(array));
+    }
+
     //Tests for the methods above
     public static void main(String[] args) {
         int[] array = {0, -1, 4, 5, -2, 6};
@@ -31,5 +69,7 @@ public class Task1_OneDimensionalArray {
 
         System.out.println(sumOfMultiples(array, -1));
         System.out.println(changeToZ(arrayD, 3));
+        System.out.println(Arrays.toString(calculateMembers(arrayD)));
+        exchangeMinMax(arrayD);
     }
 }
