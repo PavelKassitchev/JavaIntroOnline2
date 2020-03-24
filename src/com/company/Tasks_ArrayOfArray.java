bbfc29c36ab4438d3cdbea0748227d6df17f89a1
paul <pavkascool@gmail.com>
2020-03-24 15:51:49 +0300

    complete tasks to 9, arrays of arrays

--- src/com/company/Tasks_ArrayOfArray.java
+++ src/com/company/Tasks_ArrayOfArray.java
@@ -48,8 +48,8 @@ public class Tasks_ArrayOfArray {
         System.out.println();
     }
 
-    //This method generates a square matrix following the template in the task
-    public static int[][] generateMatrix(int n) {
+    //This method generates a square matrix following the template in the task 4
+    public static int[][] generateMatrix1(int n) {
         int[][] array = new int[n][n];
 
         for(int i = 1; i < n; i += 2) {
@@ -62,6 +62,86 @@ public class Tasks_ArrayOfArray {
         return array;
     }
 
+    //This method generates a square matrix following the template in the task 5
+    public static int[][] generateMatrix2(int n) {
+        int[][] array = new int[n][n];
+
+        for(int i = 0; i < n; i ++) {
+            for(int j = 0; j < n; j++) {
+                array[i][j] = i+1;
+            }
+        }
+        return array;
+    }
+
+    //This method generates a square matrix following the template in the task 6
+    public static int[][] generateMatrix3(int n) {
+        int[][] array = new int[n][n];
+
+        for(int i = 0; i < n/2; i ++) {
+            for(int j = 0; j < n; j++) {
+                if(j < i || n - 1 - j < i) {
+                    array[i][j] = 0;
+                    array[n - 1 - i][j] = 0;
+                }
+                else {
+                    array[i][j] = 1;
+                    array[n - 1 - i][j] = 1;
+                }
+            }
+        }
+        return array;
+    }
+
+    //This method generates a square matrix following the template in the task 7 and prints number of positive elements
+    public static double[][] generateMatrix4(int n) {
+        double[][] array = new double[n][n];
+        int positive = 0;
+
+        for(int i = 0; i < n; i ++) {
+            for(int j = 0; j < n; j++) {
+                array[i][j] = Math.sin((i*i - j*j) / (n + 0.0));
+                if(array[i][j] > 0) positive++;
+            }
+        }
+        System.out.println("Number of positive elements: " + positive);
+        return array;
+    }
+
+    //This method exchange 2 columns
+    public static void exchangeColumns(int[][] array, int first, int second) {
+        int size = array.length;
+        if(first < 0 || first >= size || second < 0 || second >= size) throw new IllegalArgumentException("Wrong column numbers");
+        int [] col = new int[size];
+        for(int i = 0; i < size; i++) {
+            col[i] = array[i][first];
+            array[i][first] = array[i][second];
+            array[i][second] = col[i];
+        }
+    }
+
+    //This method summarizes columns and returns column's number with max sum
+    public static int maxSum(double[][] array) {
+        double[] sums = new double[array[0].length];
+        for(int j = 0; j < array[0].length; j++) {
+            double sum = 0;
+            for(int i = 0; i < array.length; i++) {
+                sum += array[i][j];
+            }
+            sums[j] = sum;
+        }
+        double max = sums[0];
+        int index = 0;
+        for (int i = 0; i < sums.length; i++) {
+            if(sums[i] > max) {
+                max = sums[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+
     //Test method
     public static void main(String[] args) {
         int[][]intArray = {{1, 2, 3},{0, -6, 5},{4, 4, 0}};
@@ -70,9 +150,28 @@ public class Tasks_ArrayOfArray {
         printDiagonals(intArray, false);
         printRowAndColumn(intArray, 1, 1);
 
-        int[][] array =generateMatrix(4);
+        int[][] array =generateMatrix1(4);
         for(int i = 0; i < array.length; i++) {
             System.out.println(Arrays.toString(array[i]));
+        }
+
+        int[][] array2 =generateMatrix2(4);
+        for(int i = 0; i < array2.length; i++) {
+            System.out.println(Arrays.toString(array2[i]));
+        }
+
+        int[][] array3 =generateMatrix3(6);
+        for(int i = 0; i < array3.length; i++) {
+            System.out.println(Arrays.toString(array3[i]));
+        }
+
+        double[][] doubleArray = generateMatrix4(6);
+        for(int i = 0; i < doubleArray.length; i++) {
+            System.out.println(Arrays.toString(doubleArray[i]));
+        }
+        exchangeColumns(array3, 0, 1);
+        for(int i = 0; i < array3.length; i++) {
+            System.out.println(Arrays.toString(array3[i]));
         }
     }
 }
